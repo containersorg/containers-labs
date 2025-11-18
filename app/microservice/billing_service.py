406bc671167c60a0968884ab18de6976c03ddad5
jmedinar <jmedinar@gmail.com>
2025-11-17 21:55:12 -0600

    new init

--- app/microservice/billing_service.py
+++
@@ -1,9 +0,0 @@
-"""service for billing"""
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-# Additional billing functionality can be added here
-
-if __name__ == '__main__':
-    app.run(port=5004)
