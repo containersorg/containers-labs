027d145b5539bb446a779fed5e1c4c056fd02d8f
jmedinar <jmedinar@gmail.com>
2025-11-23 13:06:16 -0600

    extensions reviewed

--- tools/vsc_ext.sh
+++ tools/vsc_ext.sh
@@ -3,23 +3,28 @@
 # Email: jumedina@redhat.com
 # Date: Nov 2025
 
-EXT_FILE="extensions.txt"
+# URL pointing to the extensions file on GitHub
+EXT_URL="https://raw.githubusercontent.com/containersorg/containers-labs/refs/heads/main/tools/extensions.txt"
 
 # Cause pipelines to return the exit status of the last command that failed (if any).
 set -o pipefail
 
-echo "Installing VS Code Extensions..."
+echo "Starting VS Code Extension Installation"
 
 if ! command -v code &> /dev/null; then
     echo "ERROR: 'code' command not found. Please ensure VS Code is installed and in your PATH."
     exit 1
 fi
 
-while IFS= read -r EXTENSION_ID; do
-    if [[ -n "$EXTENSION_ID" && "${EXTENSION_ID:0:1}" != "#" ]]; then
+curl -sL "$EXT_URL" | while IFS= read -r EXTENSION_ID
+do
+    EXTENSION_ID=$(echo "$EXTENSION_ID" | xargs)
+
+    if [[ -n "$EXTENSION_ID" && "${EXTENSION_ID:0:1}" != "#" ]]
+	 then
         echo "Installing: ${EXTENSION_ID}..."
         code --install-extension "$EXTENSION_ID" --force
     fi
-done < "$EXT_FILE"
+done
 
 echo "Extension installation process complete."
