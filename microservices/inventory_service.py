1c86bd96a0f4a7701777fff17fccd790f4f3fec9
R0tt3nT0m4t0 <jumedina@redhat.com>
2026-09-13 12:13:56 -0500

    collision fix

--- microservices/inventory_service.py
+++ microservices/inventory_service.py
@@ -1,5 +1,6 @@
 """service for inventories"""
 from flask import Flask, json
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def view_inventory():
     return json.dumps({'inventory': inventory}, indent=4)
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    # app.run(port=5001)
+    app.run(port=int(os.environ.get("INVENTORY_PORT", 5001)))
