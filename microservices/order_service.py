02f43bab0086c4c2efb9acc18a66f63b840cdc63
R0tt3nT0m4t0 <jumedina@redhat.com>
2026-09-13 14:54:59 -0500

    collision fix review

--- microservices/order_service.py
+++ microservices/order_service.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 orders = []
 #INVENTORY_API_URL = "http://127.0.0.1:5001" # URL for the inventory service
-INVENTORY_API_URL = "http://127.0.0.1:" + str(os.environ.get("INVENTORY_PORT", 5001))
+INVENTORY_API_URL = f"http://127.0.0.1:{os.environ.get('INVENTORY_PORT', 5001)}"
 
 # Endpoint to place orders
 @app.route('/place_order', methods=['POST'])
