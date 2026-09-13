725afdf5de99e29a8f7cb474674119161e64880a
R0tt3nT0m4t0 <jumedina@redhat.com>
2026-09-13 18:38:45 -0500

    collision fix review

--- microservices/order_service.py
+++ microservices/order_service.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 
 # File to store orders
 ORDERS_FILE = '/data/orders.json'
-# INVENTORY_API_URL = "http://127.0.0.1:5001"
-INVENTORY_API_URL = f"http://127.0.0.1:{os.environ.get('INVENTORY_PORT', 5001)}"
+INVENTORY_API_URL = os.environ.get("INVENTORY_API_URL", "http://127.0.0.1:5001")
 
 # Create the orders file if it doesn't exist                                                   
 if not os.path.exists(ORDERS_FILE):
